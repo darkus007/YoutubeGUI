3b07bafb87fcc28b727bc6cf5cc326df69cf4dd7
Sergey <darkus007@yandex.ru>
2022-04-06 21:33:50 +0300

    Added functionality in YouTube module (download selected video or audio; download visualization)

--- youtube.py
+++ youtube.py
@@ -1,10 +1,10 @@
-""" Simple YouTube downloader the best resolution video by URL """
+""" Simple YouTube downloader video or audio by URL """
 from copy import copy
 from pytube import YouTube
 
 
 class MyYouTuBe(YouTube):
-    """ Simple YouTube downloader the best resolution video by URL """
+    """ Simple YouTube downloader video or audio by URL """
     FORMAT = {'format': None, 'qualityLabel': None, 'width': None, 'height': None, 'bitrate': None, 'fps': None,
               'audioChannels': None, 'audioSampleRate': None, 'size': None, 'itag': None}
 
@@ -36,13 +36,22 @@ class MyYouTuBe(YouTube):
 
         return self.available_streams
 
-    def download_best_video(self, path='video/') -> None:
+    def download_best_video(self, path: str = 'video/') -> None:
         """ Download the best video in progressive format
 
             :param path: path to save video (optional)
         """
         self.streams.filter(progressive=True,
                             file_extension='mp4').order_by('resolution').desc().first().download(path)
+
+    def download_video_by_itag(self, itag: int, path='video/', filename_prefix=None) -> None:
+        """ Download video or audio by itag
+
+            :param itag: video or audio by itag
+            :param path: path to save video (optional)
+            :param filename_prefix: a string that will be prepended to the filename (optional)
+        """
+        self.streams.get_by_itag(itag).download(output_path=path, filename_prefix=filename_prefix)
 
     @property
     def best_video_size(self) -> int:
@@ -52,13 +61,19 @@ class MyYouTuBe(YouTube):
 
     def on_progress(self, stream, chunk, bytes_remaining) -> None:
         """ Executed during the loading of the stream and displays the progress of its loading """
-        percent = (self.best_video_size - bytes_remaining) / self.best_video_size
-        print(f'Downloaded: {percent:.0%}', end='\r')
+        # Visualizes the download progress
+        # rows, columns = os.popen('stty size', 'r').read().split()
+        fill_width = 48
+        current_percent = (stream.filesize - bytes_remaining) / stream.filesize
+        filled_length = int(fill_width * current_percent)
+        empty_length = fill_width - filled_length
+        bar = f'| {"#" * filled_length + " " * empty_length} | '
+        print(f'Downloaded:{bar} {current_percent:.0%}', end='\n' if current_percent >= 1.0 else '\r')
 
     @staticmethod
     def on_complete(stream, path: str):
         """ Will be executed after the download is complete """
-        print("File saved as:\n" + path)
+        print("File saved as:  \n" + path)
 
     @staticmethod
     def reduce_stream_info(stream: dict, temp: dict) -> dict:
@@ -100,16 +115,53 @@ class MyYouTuBe(YouTube):
         return temp
 
 
-def main():
-    # url = 'https://www.youtube.com/watch?v=t5Bo1Je9EmE'
+if __name__ == '__main__':
+    print("Welcome to youtube video and audio downloader!")
+    choice = -1
+    while choice not in (0, 1, 2):
+        try:
+            choice = int(input("1 - to download the best video; 2 - select boot options; 0 - exit. "))
+            if choice not in (0, 1, 2):
+                print(f'Number is out of range (0 - 2)')
+        except ValueError:
+            print('Wrong value!')
+
+    if choice == 0:
+        exit()
+
     url = input('Enter the YouTube URL: ')
     video = MyYouTuBe(url)
     print(video.title)
     print(video.author)
-    print(f'Best video size {video.best_video_size} bytes')
-    # pprint(video.get_all_streams())
-    video.download_best_video()
 
+    # download the best video
+    if choice == 1:
+        print(f'Best video size {video.best_video_size} bytes')
+        video.download_best_video()
+        exit()
 
-if __name__ == '__main__':
-    main()
+    # select boot options
+    if choice == 2:
+        all_streams = video.get_all_streams()
+        len_all_streams = len(all_streams)
+
+        # print streams info
+        for i, it in zip(range(len_all_streams), all_streams):
+            print(f"{i}:\t{it['format']} | {it['qualityLabel']}({it['width']}/{it['height']}) "
+                  f"| bitrate: {it['bitrate']} | fps: {it['fps']} | Audio channels: {it['audioChannels']} "
+                  f"| Audio sample rate: {it['audioSampleRate']} | file size: {it['size']} bytes")
+
+        # choice
+        video_number = -1
+        while video_number not in range(len_all_streams):
+            try:
+                video_number = int(input(f'Enter video or audio number to download (0 to {len_all_streams}): '))
+                if video_number < 0 or video_number >= len_all_streams:
+                    print(f'Number is out of range (0 - {len_all_streams})')
+            except ValueError:
+                print('Wrong value!')
+
+        # download
+        video.download_video_by_itag(itag=all_streams[video_number]["itag"],
+                                     filename_prefix=None if not all_streams[video_number]["qualityLabel"] is None
+                                     else 'Audio ')
